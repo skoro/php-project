4e0677df27d3a591306e590408a7036480dd4f94
Oleksii Skorobogatko <skorobogatko.oleksii@gmail.com>
2023-08-02 22:37:21 +0300

    swoole already has cpu count function

--- src/helpers.php
+++ src/helpers.php
@@ -33,34 +33,6 @@ if (!function_exists('report_error')) {
     }
 }
 
-if (!function_exists('cpu_cores_count')) {
-    /**
-     * Gets CPU cores count.
-     *
-     * Supports Linux & Windows.
-     *
-     * @return int CPU core count.
-     * @throws RuntimeException When cannot get info about CPU.
-     */
-    function cpu_cores_count(): int
-    {
-        static $cores = null;
-        if ($cores !== null) {
-            return $cores;
-        }
-        if (is_file('/proc/cpuinfo')) {
-            $cpuinfo = @file_get_contents('/proc/cpuinfo');
-            if ($cpuinfo && preg_match('/^cpu cores[ \t]*:[ \t]*(\d+)$/m', $cpuinfo, $matches)) {
-                $cores = (int)$matches[1];
-            }
-        }
-        elseif (PHP_OS_FAMILY === 'Windows') {
-            $cores = (int)getenv('NUMBER_OF_PROCESSORS');
-        }
-        return $cores ?? throw new RuntimeException('Cannot get cpu cores count for your OS: ' . PHP_OS_FAMILY);
-    }
-}
-
 if (!function_exists('json_response')) {
     function json_response(mixed $data, int $statusCode = 200): \Laminas\Diactoros\Response\JsonResponse
     {
